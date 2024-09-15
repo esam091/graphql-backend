2566668e837441b9c1025e64eb2b0ec9a7b490bd
Samuel <se12345a@email.me>
2024-09-15 16:18:41 +0700

    remove unused imports

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,6 +1,5 @@
 
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
 import jwt from 'jsonwebtoken'
 import { typeDefs } from '@/schema/typeDefs.generated'
 import { resolvers } from '@/schema/resolvers.generated'
