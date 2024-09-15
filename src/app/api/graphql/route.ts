1ab95907dde7eb45adb22c7ad6ffcf0ae98e9514
Samuel <se12345a@email.me>
2024-09-15 16:15:11 +0700

    seems to work

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -13,6 +13,33 @@ const server = new ApolloServer<Context>({
   // includeStacktraceInErrorResponses: false,
 })
 
-const handler = startServerAndCreateNextHandler(server)
+const handler = startServerAndCreateNextHandler(server, {
+  context: async (req) => {
+
+    // @ts-ignore types not matching
+    const token = req.headers.get('authorization') || ''
+    try {
+      const payload = jwt.verify(
+        token.replace('Bearer ', ''),
+        process.env.JWT_SECRET!
+      )
+
+      if (
+        typeof payload !== 'object' ||
+        typeof payload.id !== 'string' ||
+        typeof payload.role !== 'string' ||
+        !(payload.role === 'user' || payload.role === 'admin')
+      ) {
+        return { currentUser: null }
+      }
+
+      return {
+        currentUser: { id: payload.id, role: payload.role as 'user' | 'admin' },
+      }
+    } catch (error) {
+      return { currentUser: null }
+    }
+  },
+})
 
 export { handler as GET, handler as POST }
